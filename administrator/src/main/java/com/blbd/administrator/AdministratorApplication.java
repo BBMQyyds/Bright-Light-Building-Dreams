464b9511b6901c37e14c0617a6926b4fe0fccd97
33698 <3369855428@qq.com>
2023-10-30 23:49:42 +0800

    项目初始化

---
+++ administrator/src/main/java/com/blbd/administrator/AdministratorApplication.java
@@ -0,0 +1,13 @@
+package com.blbd.administrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdministratorApplication.class, args);
+    }
+
+}
