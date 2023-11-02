d93ef7a43dfb1ab3554f64ff8ea0122ea24f3e93
sq <1838545301@qq.com>
2023-11-02 20:32:31 +0800

    修复了mybatisplus和springboot版本冲突问题

--- children/src/main/java/com/blbd/children/controller/TaskChildController.java
+++ children/src/main/java/com/blbd/children/controller/TaskChildController.java
@@ -1,7 +1,6 @@
 package com.blbd.children.controller;
 
 
-import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author sq
  * @since 2023-11-01
  */
-@Api(tags = "控制器-任务")
+//@Api(tags = "控制器-任务")
 @RestController
 @RequestMapping("/children/task-child")
 public class TaskChildController {
--- children/src/main/java/com/blbd/children/dao/entity/Subject.java
+++ children/src/main/java/com/blbd/children/dao/entity/Subject.java
@@ -3,15 +3,13 @@ package com.blbd.children.dao.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
 /**
  * <p>
- * 
+ *
  * </p>
  *
  * @author sq
@@ -20,7 +18,7 @@ import lombok.experimental.Accessors;
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
-@ApiModel(value="Subject对象", description="")
+//@ApiModel(value="Subject对象", description="")
 public class Subject implements Serializable {
 
     private static final long serialVersionUID = 1L;
