42aec62f6e694a4621599cda1098659d3dca7188
王山而 <346669054@qq.com>
2023-11-04 14:01:10 +0800

    child的主键声明了

--- children/src/main/java/com/blbd/children/dao/entity/Child.java
+++ children/src/main/java/com/blbd/children/dao/entity/Child.java
@@ -1,5 +1,8 @@
 package com.blbd.children.dao.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,13 +20,21 @@ import lombok.experimental.Accessors;
 @TableName("child")
 //@ApiModel(value="Child对象", description="")
 public class Child {
+    @TableId(value = "id", type = IdType.ASSIGN_UUID)
     private String id;
+    @TableField("username")
     private String username;
+    @TableField("score")
     private Integer score;
+    @TableField("password")
     private String password;
+    @TableField("name")
     private String name;
+    @TableField("grade")
     private String grade;
+    @TableField("locate")
     private String locate;
+    @TableField("duty")
     private Integer duty;
 
     public Child(String id, String username, Integer score) {
