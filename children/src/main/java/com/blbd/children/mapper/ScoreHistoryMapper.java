8ad1ba0dc8690ae506c1d4ca8de15b762c5469da
sq <1838545301@qq.com>
2023-11-05 16:11:04 +0800

    儿童端积分明细总表业务逻辑完成，视图定义可能修改

---
+++ children/src/main/java/com/blbd/children/mapper/ScoreHistoryMapper.java
@@ -0,0 +1,18 @@
+package com.blbd.children.mapper;
+
+import com.blbd.children.dao.entity.ScoreHistory;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p>
+ * VIEW Mapper 接口
+ * </p>
+ *
+ * @author sq
+ * @since 2023-11-05
+ */
+@Repository
+public interface ScoreHistoryMapper extends BaseMapper<ScoreHistory> {
+
+}
---
+++ children/src/main/java/com/blbd/children/service/impl/ScoreHistoryServiceImpl.java
@@ -0,0 +1,57 @@
+package com.blbd.children.service.impl;
+
+import com.blbd.children.dao.entity.ScoreHistory;
+import com.blbd.children.mapper.ScoreHistoryMapper;
+import com.blbd.children.service.ScoreHistoryService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * <p>
+ * 服务实现类 VIEW
+ * 积分明细
+ *      获得积分表--已批改任务
+ *      消耗积分表--订单/兑换商品：
+ *          展示当前孩子的订单列表（消耗积分表）：购买物品的图片，物品名称，物品描述，物品花费的总积分值，订单的状态
+ *      积分收支表--前二者按时间的总排序：
+ *          暂时以儿童提交任务时间为积分获得时间
+ *          平台由前端通过判断event_type自行填充
+ *
+ * 积分收支表需求：
+ *
+ * </p>
+ *
+ * @author sq
+ * @since 2023-11-05
+ */
+@Service
+public class ScoreHistoryServiceImpl extends ServiceImpl<ScoreHistoryMapper, ScoreHistory> implements ScoreHistoryService {
+    @Autowired
+    private ScoreHistoryMapper scoreHistoryMapper;
+
+    /**
+     * 查看当前孩子的积分收支表
+     */
+    @Override
+    public List<ScoreHistory> getListByChildId(String childId) {
+        HashMap<String, Object> map = new HashMap<>();
+
+        map.put("child_id",childId);
+
+        List<ScoreHistory> scoreHistories = scoreHistoryMapper.selectByMap(map);
+
+        if (scoreHistories.isEmpty()){
+            return new ArrayList<>();
+        }
+
+        return scoreHistories;
+    }
+
+
+
+}
